0e76a451aad97c8d6afd9d06257ed870bc6a6d31
hongjh <565983236@qq.com>
2016-05-04 14:09:01 +0800

    add simple PDO

---
+++ include/SimplePDO.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * 简单PDO操作类
+ * @author hongjh <565983236@qq.com>
+ */
+class SimplePDO
+{
+ 
+    protected static $_instance = null;
+    protected $dsn;
+    protected $dbh;
+
+    private function __construct($dbHost, $dbUser, $dbPasswd, $dbName, $dbCharset, $dbPort)
+    {
+        try {
+            $this->dsn = 'mysql:host=' . $dbHost . ';port=' . $dbPort . ';dbname=' . $dbName;
+            $this->dbh = new PDO($this->dsn, $dbUser, $dbPasswd);
+            // 禁用php模拟预处理
+            $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);            
+            $this->dbh->exec("set names utf8");            
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public static function getInstance($dbHost, $dbUser, $dbPasswd, $dbName, $dbCharset = 'utf8', $dbPort = '3306')
+    {
+        if (self::$_instance === null) {
+            self::$_instance = new self($dbHost, $dbUser, $dbPasswd, $dbName, $dbCharset, $dbPort);
+        }
+        return self::$_instance;
+    }
+
+}
