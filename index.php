0ea69d38e3b4a426990bdcba09570e0a36fba44b
hongjh <565983236@qq.com>
2016-05-04 18:04:31 +0800

    add batch insert demo

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'include/SimplePDO.php';
 
 $dbHost = '127.0.0.1';
@@ -10,9 +11,18 @@ $db = SimplePDO::getInstance($dbHost, $dbUser, $dbPasswd, $dbName);
 
 
 /* insert */
-$data = array(
-    'username' => 'hello killty',
-    'age'=> '5'
-);
-$db->insert('users', $data);
-/* end insert */
+$data = ['username' => 'hello killty', 'age' => '5'];
+//$db->insert('users', $data);
+
+
+/* batch insert */
+$multiData = [];
+for($i= 0; $i < 10000; $i++) {
+$multiData[] = ['username' => "hello 'skillty\"", 'age' => $i];
+}
+
+// add fail data
+//$multiData[] = ['username' => 'hello killty', 'age' => 'abc'];
+
+$db->batchInsert('users', $multiData);
+
