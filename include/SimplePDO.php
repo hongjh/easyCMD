8266fb10109610c7a6e3d7faf91493a35f12ee49
hongjh <565983236@qq.com>
2016-05-04 15:07:29 +0800

    add insert function

--- include/SimplePDO.php
+++ include/SimplePDO.php
@@ -6,7 +6,7 @@
  */
 class SimplePDO
 {
- 
+
     protected static $_instance = null;
     protected $dsn;
     protected $dbh;
@@ -17,8 +17,8 @@ class SimplePDO
             $this->dsn = 'mysql:host=' . $dbHost . ';port=' . $dbPort . ';dbname=' . $dbName;
             $this->dbh = new PDO($this->dsn, $dbUser, $dbPasswd);
             // 禁用php模拟预处理
-            $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);            
-            $this->dbh->exec("set names utf8");            
+            $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->dbh->exec("set names utf8");
         } catch (PDOException $e) {
             throw new Exception($e->getMessage());
         }
@@ -30,6 +30,26 @@ class SimplePDO
             self::$_instance = new self($dbHost, $dbUser, $dbPasswd, $dbName, $dbCharset, $dbPort);
         }
         return self::$_instance;
+    }
+
+    public function insert($table, $data)
+    {
+
+        $fields = array_keys($data);
+        $fields_values = array_values($data);
+
+        $fields_str = implode(",", $fields);
+        $fields_values_str = implode(',', array_fill(0, count($fields), '?'));
+
+        $sql = "INSERT INTO $table (" . $fields_str . ") VALUES (" . $fields_values_str . ")";
+
+        $sth = $this->dbh->prepare($sql);        
+        return $sth->execute($fields_values);
+    }
+
+    public function lastInsertId()
+    {
+        return $this->pdo->lastInsertId();
     }
 
 }
